7c5aa41893d595c1ca48d103c949570aeefbef56
Ben <thisguyistotallyben@gmail.com>
2020-04-17 01:39:51 -0400

    Models - Added models for   - Bot   - Server   - Channel   - Message

--- discordbotmanager/bots/models.py
+++ discordbotmanager/bots/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Bot(models.Model):
+    name = models.CharField(max_length=200, default='')
+
+
+class Server(models.Model):
+    server_id = models.IntegerField(default=0)
+    name = models.CharField(max_length=200, default='')
+    bot = models.ForeignKey(Bot, on_delete=models.CASCADE)
+
+
+class Channel(models.Model):
+    channel_id = models.IntegerField(default=0)
+    name = models.CharField(max_length=200, default='')
+    server = models.ForeignKey(Server, on_delete=models.CASCADE)
+
+
+class Message(models.Model):
+    channel = models.ForeignKey(Channel, on_delete=models.CASCADE)
+    username = models.CharField(max_length=200, default='')
+    screen_name = models.CharField(max_length=200, default='')
+    date = models.DateTimeField('date sent')
+    message = models.CharField(max_length=2000, default='')
+
+
